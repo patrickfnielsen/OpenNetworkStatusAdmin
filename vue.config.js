7ead79daf37feb2a3ff6fb82941826d705b47de1
Patrick Falk Nielsen <patrick.nielsen@bestseller.com>
2020-09-30 15:59:55 +0200

    Formatting

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     chainWebpack: config => {
-    config.plugin("copy").tap(([pathConfigs]) => {
-        pathConfigs.unshift({
-            from: "cfg",
-            to: "cfg"
-        });
-        return [pathConfigs]})
+        config.plugin("copy").tap(([pathConfigs]) => {
+            pathConfigs.unshift({
+                from: "cfg",
+                to: "cfg"
+            });
+            return [pathConfigs]
+        })
     },
     transpileDependencies: ["vuetify"]
 };
