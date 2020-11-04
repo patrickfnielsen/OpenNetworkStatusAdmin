6beee6424ad9b1b045d694abfc91700ceb6d32b9
Patrick Falk Nielsen <patrick.nielsen@bestseller.com>
2020-11-04 10:25:01 +0100

    fix: null date would return "Invalid Date"

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,11 @@
 import dayjs from 'dayjs';
 
 function formatDate(date) {
+    if (date == null) {
+        return "N/A"
+        
+    }
+        
     return dayjs(date).format("DD/MM/YYYY HH:mm:ss UTC");
 }
 
